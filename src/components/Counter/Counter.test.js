1f6896f6d6d9bf0421d1dbc06b670ae0f41120f3
rrauf8178 <rrauf8178@gmail.com>
2023-02-04 16:52:19 +0400

    Cleaned test code for counter

--- src/components/Counter/Counter.test.js
+++ src/components/Counter/Counter.test.js
@@ -1,20 +1,24 @@
-import {render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
-import Counter from './index';
+import Counter from "./index";
 
-test('increase btn', () =>{
-  render (<Counter/>);
-  const count = screen.getByText("0");
-  const increaseBtn = screen.getByText("Increase");
-  userEvent.click(increaseBtn);
-  expect(count).toHaveTextContent("1")
-});
-test('decrease btn', () =>{
-    render (<Counter/>);
-    const count = screen.getByText("0");
-    const decreaseBtn = screen.getByText("Decrease");
-    userEvent.click(decreaseBtn);
-    expect(count).toHaveTextContent("-1")
+describe("Counter Tests", () => {
+    let increaseBtn, decreaseBtn, count;
+
+  beforeEach(() => {
+    render(<Counter />);
+     increaseBtn = screen.getByText("Increase");
+     decreaseBtn = screen.getByText("Decrease");
+     count = screen.getByText("0");
   });
 
+  test("increase btn", () => {
+    userEvent.click(increaseBtn);
+    expect(count).toHaveTextContent("1");
+  });
+  test("decrease btn", () => {
+    userEvent.click(decreaseBtn);
+    expect(count).toHaveTextContent("-1");
+  });
+});
